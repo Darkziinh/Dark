644bcdd08f3f208c172064562a10ef26e86643da
Darkziinh <76751583+Darkziinh@users.noreply.github.com>
2021-01-02 09:26:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,4 @@ wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-oc
 npm install
 
 echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+npm start
